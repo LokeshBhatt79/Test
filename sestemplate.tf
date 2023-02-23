7b123b2aaf6fe6162701cd57babd52a5cedc471d
LokeshBhatt79 <36851159+LokeshBhatt79@users.noreply.github.com>
2023-02-23 19:00:29 +0530

    Update sestemplate.tf

--- sestemplate.tf
+++ sestemplate.tf
@@ -4,3 +4,9 @@ resource "aws_ses_template" "MyTemplate" {
   html    = "<h1>Hello {{name}},</h1><p>Your favorite animal is {{favoriteanimal}}.</p>"
   text    = "Hello {{name}},\r\nYour favorite animal is {{favoriteanimal}}."
 }
+resource "aws_ses_template" "MyTemplate" {
+  name    = "MyTemplate"
+  subject = "Greetings, {{name}}!"
+  html    = "<h1>Hello {{name}},</h1><p>Your favorite animal is {{favoriteanimal}}.</p>"
+  text    = "Hello {{name}},\r\nYour favorite animal is {{favoriteanimal}}."
+}
